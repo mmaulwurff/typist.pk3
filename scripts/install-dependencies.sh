e11bb973156fb2775205583bb2393b0de132b22c
Alexander <mmaulwurff@gmail.com>
2019-07-06 01:24:55 +0700

    add signing of gzdoom repository

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-add-apt-repository "deb http://debian.drdteam.org/ stable multiverse"
+add-apt-repository 'deb http://debian.drdteam.org/ stable multiverse'
+wget -O- http://debian.drdteam.org/drdteam.gpg | sudo apt-key add -
 apt-get update
 apt-get install gzdoom aspell zip
