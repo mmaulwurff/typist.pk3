83b7f74f4a8f3b2efa2c3542a6abcebc28acb155
Alexander <mmaulwurff@gmail.com>
2019-07-27 20:39:49 +0700

    add pylint3 and shellcheck to CI dependencies installation

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# This script installs packages that Typist.pk3 build process depends on.
+# This script is intended to be run on CI server.
+
 add-apt-repository 'deb http://debian.drdteam.org/ stable multiverse'
-wget -O- http://debian.drdteam.org/drdteam.gpg | sudo apt-key add -
+wget -O- http://debian.drdteam.org/drdteam.gpg | apt-key add -
 apt-get update
-apt-get install gzdoom aspell aspell-en zip freedoom xvfb
+apt-get install gzdoom aspell aspell-en zip freedoom xvfb pylint3 shellcheck
