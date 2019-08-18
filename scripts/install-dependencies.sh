cbd3202fbe214f0676326e992d6c4e363a307744
Alexander <mmaulwurff@gmail.com>
2019-08-18 19:56:32 +0700

    make the travis user the owner of the config

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -11,3 +11,4 @@ apt-get install gzdoom aspell aspell-en zip freedoom xvfb pylint3 shellcheck
 # Disable GZDoom hardware survey:
 mkdir --parents ~/.config/gzdoom
 echo -e "[GlobalSettings]\nsys_statsenabled=1" > ~/.config/gzdoom/gzdoom.ini
+chown travis ~/.config/gzdoom/gzdoom.ini
