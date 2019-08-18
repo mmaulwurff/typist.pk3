7246835aca25db334c70a8abb991cdd4da44c767
Alexander <mmaulwurff@gmail.com>
2019-08-18 16:09:50 +0700

    add GZDoom version print to test script

--- scripts/norun_gzdoom.sh
+++ scripts/norun_gzdoom.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# This script runs GZDoom without any mods, just a sanity check.
+# This script prints GZDoom version and runs GZDoom without any mods, just a
+# sanity check.
+#
 # The output is filtered according to filter_file.
 #
 # Usage:
 # ./scripts/norun_gzdoom.sh <filter_file>
 
 filter_file=$1
+
+apt-cache policy gzdoom
 
 time gzdoom -iwad /usr/share/games/doom/freedoom2.wad -norun -nosound 2>&1 |\
     grep -vf "$filter_file" |\
