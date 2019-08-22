3966b5bf247272c9b5ff8c57b454a84947371418
Alexander <mmaulwurff@gmail.com>
2019-08-22 15:07:47 +0700

    add preload to CI dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,11 +2,14 @@
 
 # This script installs packages that Typist.pk3 build process depends on.
 # This script is intended to be run on CI server.
+#
+# CI server is configured as Ubuntu Xenial, so packages may be different
+# for your distributive.
 
 add-apt-repository 'deb http://debian.drdteam.org/ stable multiverse'
 wget -O- http://debian.drdteam.org/drdteam.gpg | apt-key add -
 apt-get update
-apt-get install gzdoom aspell aspell-en zip freedoom xvfb pylint3 shellcheck
+apt-get install gzdoom aspell aspell-en zip freedoom xvfb pylint3 shellcheck preload
 
 # Disable GZDoom hardware survey:
 mkdir --parents ~/.config/gzdoom
